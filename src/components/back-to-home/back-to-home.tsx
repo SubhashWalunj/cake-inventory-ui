885b418c5cf91345de2b011a82db56b367a95ecf
Subhash Walunj <subhash.walunj@theaa.com>
2021-07-21 17:16:46 +0100

    back-to-home padding applied

--- src/components/back-to-home/back-to-home.tsx
+++ src/components/back-to-home/back-to-home.tsx
@@ -1,12 +1,13 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './back-to-home.css';
 
 function BackToHome() {
     return (
-        <>
+        <div className="back-to-home-container">
             <NavLink to='/'><ArrowLeftOutlined />  <span>Back to home</span></NavLink>
-        </>
+        </div>
     );
 }
 
